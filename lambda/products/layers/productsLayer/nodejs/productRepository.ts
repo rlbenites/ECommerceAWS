30f1ec4c8debf457fba522cb7ecaea8958528700
Rafael Ramos Benites <rafael.benites@lli9.com.br>
2026-04-15 10:07:23 -0300

    Salvando alterações

---
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -0,0 +1,73 @@
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import { v4 as uuid } from "uuid";
+
+export interface Product {
+  id: string;
+  productName: string;
+  code: string;
+  price: number;
+  model: string;
+}
+
+export class ProductRepository {
+  private ddbClient: DocumentClient;
+  private productsDdb: string;
+
+  constructor(ddbClient: DocumentClient, productsDdb: string) {
+    this.ddbClient = ddbClient;
+    this.productsDdb = productsDdb;
+  }
+
+  async getAllProduct(): Promise<Product[]> {
+    const data = await this.ddbClient
+      .scan({
+        TableName: this.productsDdb,
+      })
+      .promise();
+    return data.Items as Product[];
+  }
+
+  async getProductById(productId: string): Promise<Product[]> {
+    const data = await this.ddbClient
+      .get({
+        TableName: this.productsDdb,
+        Key: {
+          id: productId,
+        },
+      })
+      .promise();
+    if (data.Item) {
+      return data.Item as Product[];
+    } else {
+      throw new Error("Product not found");
+    }
+  }
+
+  async creat(product: Product): Promise<Product> {
+    product.id = uuid();
+    await this.ddbClient
+      .put({
+        TableName: this.productsDdb,
+        Item: product,
+      })
+      .promise();
+    return product;
+  }
+  async deleteProduct(productId: string): Promise<Product> {
+    productId = uuid();
+    const data = await this.ddbClient
+      .delete({
+        TableName: this.productsDdb,
+        Key: {
+          id: productId,
+        },
+        ReturnValues: "ALL_OLD",
+      })
+      .promise();
+    if (data.Attributes) {
+      return data.Attributes as Product;
+    } else {
+        throw new Error ('Product not found')
+    }
+  }
+}
